c9988a6b700c3ba280138e152da5e4560ae5d7c7
Alex Hernán Laulhe <109404316+ahlaulhee@users.noreply.github.com>
2023-05-29 10:11:57 -0300

    changes some firebase settings and started cases.ts module

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -2,15 +2,16 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
+require("dotenv").config();
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAjZtKRK3PfxgHFwlrHtppVeVZB9iaX4U8",
-  authDomain: "portfolio-term.firebaseapp.com",
-  projectId: "portfolio-term",
-  storageBucket: "portfolio-term.appspot.com",
-  messagingSenderId: "167067445957",
-  appId: "1:167067445957:web:a8881ca787444bccf89eac",
-  measurementId: "G-XJTCQDHQ82",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
